e427746dfabf5be04aa063b37129960a108e9968
Pedro Jesus Hincapie Garcia <pedro.hincapie@ceiba.com.co>
2020-02-05 18:52:38 -0500

    Inicializacion del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+  nombre: string;
+  apellido = 'Hincapie';
+  items = ['holaaaa', 'mundo'];
 }
